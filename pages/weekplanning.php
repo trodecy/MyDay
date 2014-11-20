03b05a456221d462d05e6f48e92b4d837f0d4729
trodecy <peter_willem@hotmail.com>
2014-11-20 13:20:39 +0100

    table

--- pages/weekplanning.php
+++ pages/weekplanning.php
@@ -5,6 +5,14 @@ $functions = New Functions();
 $dag = date(D);
 $date = date("d-m-Y");
 
+echo "<table>";
+echo "<tr>";
+echo "<td> lala </td>";
+echo "<td> lala2 </td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td> lala3 </td>";
+echo "</tr>";
 for($i=0;$i<7;$i++)
 {
     echo "<div>";
