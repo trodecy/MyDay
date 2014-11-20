7397f69d5c94105d69a063f84c2061082d0b1285
trodecy <peter_willem@hotmail.com>
2014-11-20 13:29:01 +0100

    table

--- pages/weekplanning.php
+++ pages/weekplanning.php
@@ -4,17 +4,19 @@ $functions = New Functions();
 
 $dag = date(D);
 $date = date("d-m-Y");
-
-echo "<table border='1' style='width:100%'>";
-echo "<tr>";
-echo "<td> maandag </td>";
-echo "<td> dinsdag </td>";
-echo "<td> woensdag </td>";
-echo "<td> donderdag </td>";
-echo "<td> vrijdag </td>";
-echo "<td> zaterdag </td>";
-echo "</tr>";
-echo "<tr>";
+?>
+<table border="1" style="width:100%">
+<tr>
+<td> maandag </td>
+<td> dinsdag </td>
+<td> woensdag </td>
+<td> donderdag </td>
+<td> vrijdag </td>
+<td> zaterdag </td>
+</tr>
+<tr>
+    
+<?php
 for($i=0;$i<7;$i++)
 {
     echo "<td>";
