99b4afb8ca39e417307f877c16f11cdb983dca96
trodecy <peter_willem@hotmail.com>
2014-11-20 15:18:28 +0100

    ffff

--- functions.php
+++ functions.php
@@ -71,7 +71,7 @@ class Functions
     
     function getSingleTaskByDate( $taskid, $date )
     {
-        //echo "SELECT * FROM Tasks WHERE id = " . $taskid . " AND date = '" . $date . "'" ;
+        echo "SELECT * FROM Tasks WHERE id = " . $taskid . " AND date = '" . $date . "'" ;
         $result = $this->db->query( "SELECT * FROM Tasks WHERE id = " . $taskid . " AND date = '" . $date . "'");
         $task = mysqli_fetch_assoc( $result );
         return $task;
