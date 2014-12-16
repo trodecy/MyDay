70918b2d4aa6d21fc7575912824272ab8c1b9a5f
Jikizuari <Jikizuari@gmail.com>
2014-12-16 13:58:58 +0100

    viewtask

--- pages/viewtask.php
+++ pages/viewtask.php
@@ -16,6 +16,7 @@
 <div class="content col-md-10">
 
     <div id="taskbar">
+        <h1> Taak bekijken: <?php echo $task_name; ?> </h1>
         <div class="btn-group buttons">
             <a href="?page=deletetask&id=<?php echo $task_id; ?>">
                 <button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></button>
