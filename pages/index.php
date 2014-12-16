9166756a3a5cd8d869daddc67cda21e05afd3b3e
Jikizuari <Jikizuari@gmail.com>
2014-12-16 14:04:06 +0100

    index

--- pages/index.php
+++ pages/index.php
@@ -1,9 +1,15 @@
 <!-- Content part -->
 <div class="content col-md-10">
 
+	<div id="taskbar">
+        <h1> Index </h1>
+    </div>
+
     <main id="main">
 
-        <h2> Index </h2>
+    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quae tamen a te agetur non melior, quam illae sunt, quas interdum optines. Graecum enim hunc versum nostis omnes-: Suavis laborum est praeteritorum memoria. Illa argumenta propria videamus, cur omnia sint paria peccata. Sed tempus est, si videtur, et recta quidem ad me. Beatus sibi videtur esse moriens. Quibus ego vehementer assentior. Erit enim mecum, si tecum erit. Duo enim genera quae erant, fecit tria. Duo Reges: constructio interrete. Ut pulsi recurrant? </p>
+
+		<p>Eam tum adesse, cum dolor omnis absit; Quia nec honesto quic quam honestius nec turpi turpius. Solum praeterea formosum, solum liberum, solum civem, stultost; Cur igitur, cum de re conveniat, non malumus usitate loqui? Quis istud, quaeso, nesciebat? </p>
 
     </main>
 
